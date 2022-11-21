88dec6d1ee88ecd3ad5bb3b3570e0d65ca452805
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-11-21 17:56:24 -0300

    desenvolvendo requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
-console.log("Olá Mundo")
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+
+
+
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.order}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                                   
+                </ol>
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>
+        </li>
+        `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    })
